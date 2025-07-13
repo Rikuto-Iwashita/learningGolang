429dc922c0cb47edd2ebcac2b2dd2cec23e4e8e4
Rikuto Iwashita <02rikuto18@gmail.com>
2025-07-13 12:26:01 +0900

    実践問題

--- greeting.go
+++ greeting.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-type User struct {
+type Student struct {
 	name string
 }
 
-func (b User) cal(weight, height float64) (result float64) {
-	result = weight / height / height * 10000
+func (s Student) calAvg(data []float64) (avgResult float64) {
+	sum := 0.0
+	for i := 0; i < len(data); i++ {
+		sum += data[i]
+	}
+
+	avgResult = sum / float64(len(data))
+	return
+}
+
+func (s Student) judge(avg float64) (judgeResult string) {
+	if avg >= 60 {
+		judgeResult = "passed"
+	} else {
+		judgeResult = "failed"
+	}
 	return
 }
 
 func main() {
-	user01 := User{"ri"}
-	fmt.Println(user01.name, user01.cal(54, 165))
+	a001 := Student{"sato"}
+	data := []float64{70, 65, 50, 90, 30}
+	var avg float64 = a001.calAvg(data)
+	result := a001.judge(avg)
+	fmt.Println(avg)
+	fmt.Println(a001.name + " " + result)
 }
